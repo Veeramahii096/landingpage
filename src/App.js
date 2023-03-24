bf649fe7b467a51ec6f6370da6454ed6ef98d177
VeerasamyM <veerabca09@gmail.com>
2023-03-24 23:46:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import About from './componets/About';
+import Footer from './componets/Footer';
+import Hero from './componets/Hero';
+import Navbar from './componets/Navbar';
+import Services from './componets/Services';
+import Tours from './componets/Tours';
+
+function App() {
+  return (<>
+  <Navbar/>
+  <Hero/>
+  <About/>
+  <Services/>
+  <Tours/>
+  <Footer/></>
+  
+  );
+}
+
+export default App;
---
+++ src/componets/data.js
@@ -0,0 +1,62 @@
+import tour1 from '../images/tour-1.jpeg'
+import tour2 from '../images/tour-2.jpeg'
+import tour3 from '../images/tour-3.jpeg'
+import tour4 from '../images/tour-4.jpeg'
+export const pageLinks=[
+    {
+        id:1,href:'#home' ,text:'home'
+    },
+    {
+        id:2,href:'#about' ,text:'about'
+    },
+    {
+        id:3,href:'#services' ,text:'services'
+    },
+    {
+        id:4,href:'#tours' ,text:'tours'
+    },
+    
+]
+export const socialLinks=[{id:1,href:'https://www.facebook.com' ,icon:'fab fa-facebook'},
+{id:2,href:'https://www.twitter.com' ,icon:'fab fa-twitter'},
+{id:3,href:'https://www.instagram.com' ,icon:'fab fa-instagram'},];
+
+export const service=[{
+    id:1,icon:'fas fa-wallet fa-fw',title:'saving money',
+    text:'Lorem ipsum toler cit amed connected atypical elit.Aspirants, office.'
+},
+{
+    id:2,icon:'fas fa-tree fa-fw',title:'endless hiking',
+    text:'Lorem ipsum toler cit amed connected atypical elit.Aspirants, office.'
+},
+{
+    id:3,icon:'fas fa-socks fa-fw',title:'amazing comfort',
+    text:'Lorem ipsum toler cit amed connected atypical elit.Aspirants, office.'
+}];
+
+export const tours=[{ id:1,image:tour1,date:'auguest 26th,2020',title:'Tibet Adventure',
+info:'Lorem Ipsum Toler Cit Amed, Consecter Adphysising Elit.CommunVide tempore vaulted maxim reprehenderit ume quodExercise, Q Corporis.',
+location:'china',
+duration:6,
+cost:2100
+
+},
+{ id:2,image:tour2,date:'october 1th, 2020',title:'best of java',
+info:'Lorem Ipsum Toler Cit Amed, Consecter Adphysising Elit.CommunVide tempore vaulted maxim reprehenderit ume quodExercise, Q Corporis.'
+,
+location:'indonesia',
+duration:11,
+cost:1400
+},
+{ id:3,image:tour3,date:'september 15th, 2020',title:'explore hong kon',
+info:'Lorem Ipsum Toler Cit Amed, Consecter Adphysising Elit.CommunVide tempore vaulted maxim reprehenderit ume quodExercise, Q Corporis.'
+,location:'hong kong',
+duration:8,
+cost:5000
+},
+{ id:4,image:tour4,date:'december 5th, 2019',title:'kenya highlights',
+info:'Lorem Ipsum Toler Cit Amed, Consecter Adphysising Elit.CommunVide tempore vaulted maxim reprehenderit ume quodExercise, Q Corporis.'
+,location:'kenya',
+duration:20,
+cost:3300
+},]
